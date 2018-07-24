53fe89c90a0ea06d5482c15d49fc4c8eec0b6633
fenghanxu <1162851277@qq.com>
2018-07-24 17:20:38 +0800

    Bool

--- Sources/Number/Bool.swift
+++ Sources/Number/Bool.swift
@@ -12,4 +12,18 @@ public extension Bool {
   public var int: Int { return self ? 1: 0 }
   /// Bool转String   value: "1": "0"
   public var string: String { return description }
+  /// 取反
+  public var toggled: Bool { return !self }
+  
+  /// 转换: CGFloat.
+  public var cgFloat: CGFloat { return CGFloat(self.int) }
+  
+}
+
+extension Bool{
+  /// 取反
+  @discardableResult public mutating func toggle() -> Bool {
+    self = !self
+    return self
+  }
 }
